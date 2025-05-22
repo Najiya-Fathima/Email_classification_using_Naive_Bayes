a0f9971fa3973f1e7e7b62db7b6eb898e33c39ab
Najiya Fathima O P K <najiya.ds24@duk.ac.in>
2025-05-23 02:34:46 +0530

    Add imports and clean function

--- app.py
+++ app.py
@@ -1,5 +1,69 @@
 import streamlit as st
 import joblib
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import email
+import string
+from bs4 import BeautifulSoup
+import string
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+import os
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_recall_curve 
+
+stemmer = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
+class email_to_clean_text(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+    def fit(self, X, y=None): 
+        return self
+    def transform(self, X):
+        text_list = []
+        for mail in X:
+            b = email.message_from_string(mail)
+            body = ""
+
+            if b.is_multipart():
+                for part in b.walk():
+                    ctype = part.get_content_type()
+                    cdispo = str(part.get('Content-Disposition'))
+
+                    # skip any text/plain (txt) attachments
+                    if ctype == 'text/plain' and 'attachment' not in cdispo:
+                        body = part.get_payload(decode=True)  # get body of email
+                        break
+            # not multipart - i.e. plain text, no attachments, keeping fingers crossed
+            else:
+                body = b.get_payload(decode=True) # get body of email
+            
+            soup = BeautifulSoup(body, "html.parser") #get text from body (HTML/text)
+            text = soup.get_text().lower()
+            text = re.sub(r'(https|http)?:\/\/(\w|\.|\/|\?|\=|\&|\%)*\b', '', text, flags=re.MULTILINE) #remove links
+            text = re.sub(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', '', text, flags=re.MULTILINE) #remove email addresses
+            text = text.translate(str.maketrans('', '', string.punctuation)) # remove punctuation
+            text = ''.join([i for i in text if not i.isdigit()]) # remove digits
+            stop_words = stopwords.words('english')
+
+            words_list = [w for w in text.split() if w not in stop_words] # remove stop words
+            words_list = [lemmatizer.lemmatize(w) for w in words_list] #lemmatization
+            words_list = [stemmer.stem(w) for w in words_list] #Stemming
+
+            text_list.append(' '.join(words_list))
+        return text_list
 
 # Load the pre-trained pipeline:
 pipeline_filename = "email_classifier_naive.joblib" 
